df90bf3b963c88d84f7ef42ad3066a6c98a60fe1
MisaoDev <69985862+MisaoDev@users.noreply.github.com>
2021-12-26 19:15:29 -0300

    Initial commit

---
+++ app/src/main/java/cl/inacap/tdis08/sapo/captivemonitor/SetNameIconActivity.java
@@ -0,0 +1,14 @@
+package cl.inacap.tdis08.sapo.captivemonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetNameIconActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_name_icon);
+    }
+}
