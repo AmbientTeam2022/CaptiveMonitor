ba0a3c18dc27ff216e54395e8bd1039f827be8a6
Fabian <fabianzavala505@gmail.com>
2021-12-28 10:49:02 -0300

    botones

--- app/src/main/java/cl/inacap/tdis08/sapo/captivemonitor/SelectSpeciesActivity.java
+++ app/src/main/java/cl/inacap/tdis08/sapo/captivemonitor/SelectSpeciesActivity.java
@@ -2,7 +2,9 @@ package cl.inacap.tdis08.sapo.captivemonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SelectSpeciesActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class SelectSpeciesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_species);
+    }
+
+
+    public void comenzarSapitos(View view) {
+        Intent intent = new Intent(this, MonitorActivity.class);
+        startActivity(intent);
+    }
+
+    public void comenzarPersonalizar(View view) {
+        Intent intent = new Intent(this, SetParamsActivity.class);
+        startActivity(intent);
     }
 }
